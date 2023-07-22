eedc3fd8cdceda60d34c1827e3e0bb3b91453bec
jahids-web <jahiduddin922@gmail.com>
2023-07-22 21:45:05 +0600

    Add DataContext ApplicationDbContext

---
+++ GroceryManagement.Api/DLL/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
