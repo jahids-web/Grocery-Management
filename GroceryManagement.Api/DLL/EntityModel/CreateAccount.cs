b24be9d217a5b41877a51fec0eb3aa3c16bb3ea7
jahids-web <jahiduddin922@gmail.com>
2023-07-22 22:16:40 +0600

    Add DataTable

--- GroceryManagement.Api/DLL/EntityModel/CreateAccount.cs
+++ GroceryManagement.Api/DLL/EntityModel/CreateAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,16 @@ namespace DLL.EntityModel
 {
     public class CreateAccount
     {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int PhoneNumber { get; set; }   
+
+        [Required]
+        public string Password { get; set; }
+
     }
 }
--- GroceryManagement.Api/DLL/EntityModel/Login.cs
+++ GroceryManagement.Api/DLL/EntityModel/Login.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,13 @@ namespace DLL.EntityModel
 {
     public class Login
     {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
     }
 }
