d20d3df87e31fe2b93e8e72934cce610cc0d8000
jahids-web <jahiduddin922@gmail.com>
2023-07-22 23:06:14 +0600

    Add DLL DLLDependency Class

---
+++ GroceryManagement.Api/DLL/DLLDependency.cs
@@ -0,0 +1,23 @@
+﻿using DLL.DataContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+    public class DLLDependency
+    {
+        public static void AllDependency(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                   options.UseSqlServer(configuration.GetConnectionString("GroceryDatabase")));
+
+            //services.AddTransient<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
