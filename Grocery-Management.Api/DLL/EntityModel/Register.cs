44a7ea8a11c4d41c792c8917ca09602f4fa90ac4
jahids-web <jahiduddin922@gmail.com>
2023-07-25 22:56:31 +0600

    Add EntityModel Register

---
+++ Grocery-Management.Api/DLL/EntityModel/Register.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.EntityModel
+{
+    public class Register
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
