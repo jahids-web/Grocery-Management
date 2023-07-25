3bd86449836fb2a7c5660ffec5ecd2050810e83a
jahids-web <jahiduddin922@gmail.com>
2023-07-25 23:26:03 +0600

    Add Bll Class

---
+++ Grocery-Management.Api/BLL/BLLDependency.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BLL
+{
+    public class BLLDependency
+    {
+        public static void AllDependency(IServiceCollection services, IConfiguration configuration)
+        {
+           
+        }
+    }
+}
