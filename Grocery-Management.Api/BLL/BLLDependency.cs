b60002bfc12894c276d45cfe5baead60490b1351
jahids-web <jahiduddin922@gmail.com>
2023-08-02 23:37:49 +0600

    Add Some Dependency

--- Grocery-Management.Api/BLL/BLLDependency.cs
+++ Grocery-Management.Api/BLL/BLLDependency.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using BLL.Services;
+using BLL.ViewModel;
+using FluentValidation;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BLL
@@ -7,7 +10,17 @@ namespace BLL
     {
         public static void AllDependency(IServiceCollection services, IConfiguration configuration)
         {
-           
+            services.AddTransient<ILoginService, LoginService>();
+            services.AddTransient<IRegisterService, RegisterService>();
+
+            AllFluentValidationDependency(services);
+        }
+
+        public static void AllFluentValidationDependency(IServiceCollection services)
+        {
+            services.AddScoped<IValidator<RegisterViewModel>, RegisterViewModelValidator>();
+            services.AddScoped<IValidator<LoginViewModel>, LoginViewModelValidator>();
+        
         }
     }
 }
