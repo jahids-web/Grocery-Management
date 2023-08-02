30aa2de9240c264e7a9993edf04a8355d5274d12
jahids-web <jahiduddin922@gmail.com>
2023-08-02 23:30:50 +0600

    Add Middleware ExceptionMiddleware.cs

---
+++ Grocery-Management.Api/Grocery-Management.Api/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,69 @@
+﻿//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Http;
+//using Newtonsoft.Json;
+//using System.Net;
+//using System.Threading.Tasks;
+//using System;
+
+//namespace Grocery_Management.Api.Middleware
+//{
+//    public class ExceptionMiddleware
+//    {
+//        private readonly RequestDelegate _next;
+//        private readonly IWebHostEnvironment _env;
+
+//        public ExceptionMiddleware(RequestDelegate next, IWebHostEnvironment env)
+//        {
+//            _next = next;
+//            _env = env;
+//        }
+
+//        public async Task InvokeAsync(HttpContext context)
+//        {
+//            try
+//            {
+//                await _next(context);
+//            }
+//            catch (Exception ex)
+//            {
+//                await HandleExtensionAsync(context, ex, _env);
+//            }
+
+
+//            //after this line
+//        }
+
+//        public async Task HandleExtensionAsync(HttpContext context, Exception exception, IWebHostEnvironment env)
+//        {
+//            var employee = HttpStatusCode.InternalServerError;
+//            var errors = new ApiErrorResponse()
+//            {
+//                StatusCode = (int)employee
+//            };
+
+//            if (_env.IsDevelopment())
+//            {
+//                errors.Details = exception.StackTrace;
+//            }
+//            else
+//            {
+//                errors.Details = exception.Message;
+//            }
+
+//            switch (exception)
+//            {
+//                case ApplicationValidationException e:
+//                    errors.Message = e.Message;
+//                    errors.StatusCode = (int)HttpStatusCode.UnprocessableEntity;
+//                    break;
+//                default:
+//                    errors.Message = "Something is wrong in our system";
+//                    break;
+//            }
+//            var result = JsonConvert.SerializeObject(errors);
+//            context.Response.ContentType = "application/json";
+//            context.Response.StatusCode = errors.StatusCode;
+//            await context.Response.WriteAsync(result);
+//        }
+//    }
+//}
