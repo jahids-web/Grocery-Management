7486b27a44ae30615fd5e3cfc27d0befc488df41
jahids-web <jahiduddin922@gmail.com>
2023-08-02 23:34:09 +0600

    Add UnitOfWork Class

---
+++ Grocery-Management.Api/DLL/Repositories/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Repositories
+{
+    public interface IUnitOfWork
+    {
+        ILoginRepository LoginRepository { get; }
+        IRegisterRepository RegisterRepository { get; }
+
+        Task<bool> SavaChangesAsync();
+    }
+}
