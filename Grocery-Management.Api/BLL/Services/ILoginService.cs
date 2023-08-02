49efa3d64b5cb6d7df7787508305c487e1fccfaf
jahids-web <jahiduddin922@gmail.com>
2023-08-02 23:36:22 +0600

    Add Some Dependency

--- Grocery-Management.Api/BLL/Services/ILoginService.cs
+++ Grocery-Management.Api/BLL/Services/ILoginService.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using BLL.ViewModel;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BLL.Services
 {
     public interface ILoginService
     {
+        Task<Login> InsertAsync(LoginViewModel request);
+        Task<List<Login>> GetAllAsync();
+    }
+
+    public class LoginService : ILoginService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+       
+
+        public LoginService(IUnitOfWork _unitOfWork) 
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<List<Login>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Login> InsertAsync(LoginViewModel request)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
