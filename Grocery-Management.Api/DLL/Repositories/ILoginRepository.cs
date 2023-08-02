7bbe350e871b8b0aec2d4f65a446774c4d21f580
jahids-web <jahiduddin922@gmail.com>
2023-08-02 23:33:10 +0600

    Name Changes

---
+++ Grocery-Management.Api/DLL/Repositories/ILoginRepository.cs
@@ -0,0 +1,23 @@
+﻿using DLL.DataContext;
+using DLL.EntityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Repositories
+{
+    public interface ILoginRepository : IRepositoryBase<Login>
+    {
+    }
+
+    public class LoginRepository : RepositoryBase<Login>, ILoginRepository 
+    { 
+        public LoginRepository(ApplicationDbContext context) : base(context) 
+        { 
+        
+        }
+    }
+
+}
---
+++ Grocery-Management.Api/DLL/Repositories/IRegisterRepository.cs
@@ -0,0 +1,24 @@
+﻿using DLL.DataContext;
+using DLL.EntityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Repositories
+{
+    public interface IRegisterRepository : IRepositoryBase<Register>
+    {
+
+    }
+
+    public class RegisterRepository : RepositoryBase<Register>, IRegisterRepository
+    {
+        public RegisterRepository(ApplicationDbContext context) : base(context) 
+        { 
+        
+        }
+    }
+
+}
