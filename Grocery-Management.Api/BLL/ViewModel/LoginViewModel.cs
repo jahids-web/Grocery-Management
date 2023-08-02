20306b2c3175c00028d46860a36bcb8122fc9b14
jahids-web <jahiduddin922@gmail.com>
2023-08-02 23:37:16 +0600

    Add Valetesion

--- Grocery-Management.Api/BLL/ViewModel/LoginViewModel.cs
+++ Grocery-Management.Api/BLL/ViewModel/LoginViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,20 @@ namespace BLL.ViewModel
 {
     public class LoginViewModel
     {
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+
+    public class LoginViewModelValidator : AbstractValidator<LoginViewModel>
+    {
+        public LoginViewModelValidator()
+        {
+            RuleFor(x => x.UserId).NotNull().NotEmpty();
+            RuleFor(x => x.UserName).NotNull().NotEmpty();
+            RuleFor(x => x.Password).NotNull().NotEmpty();
+        }
     }
 }
