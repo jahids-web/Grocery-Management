172776bfb2b98c2ce46d5954a1e15141dc0a7160
jahids-web <jahiduddin922@gmail.com>
2023-08-02 23:36:12 +0600

    Add Some Dependency

--- Grocery-Management.Api/BLL/Services/IRegisterService.cs
+++ Grocery-Management.Api/BLL/Services/IRegisterService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BLL.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,20 @@ namespace BLL.Services
 {
     public interface IRegisterService
     {
+        Task<Register> InsertAsync(RegisterViewModel viewModel);
+    }
+
+    public class RegisterService : IRegisterService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public RegisterService(IUnitOfWork unitOfWork) 
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<Register> InsertAsync(RegisterViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
