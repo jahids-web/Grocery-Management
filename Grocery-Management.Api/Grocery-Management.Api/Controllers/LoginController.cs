424d25acf3e13509ee913db561068237cdb42b9a
jahids-web <jahiduddin922@gmail.com>
2023-08-04 00:11:00 +0600

    Add Some Method

--- Grocery-Management.Api/Grocery-Management.Api/Controllers/LoginController.cs
+++ Grocery-Management.Api/Grocery-Management.Api/Controllers/LoginController.cs
@@ -1,14 +1,11 @@
 ﻿using BLL.Services;
 using BLL.ViewModel;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace Grocery_Management.Api.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class LoginController : ControllerBase
+    public class LoginController : MainController
     {
         private readonly ILoginService _loginService;
 
@@ -17,7 +14,35 @@ namespace Grocery_Management.Api.Controllers
             _loginService = loginService;
         }
 
-        //[HttpPost]
-        //public async Task<IActionResult> Insert(LoginViewModel request)
+        [HttpPost]
+        public async Task<IActionResult> Insert(LoginViewModel request)
+        {
+            var result = await _loginService.InsertAsync(request);
+            return Ok(result);
+        }
+
+        [HttpGet("userId")]
+        public async Task <IActionResult> GetA(int userId)
+        {
+            return Ok (await _loginService.GetAAsync(userId));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _loginService.GetAllAsync());
+        }
+
+        [HttpPut("userId")]
+        public async Task<IActionResult> Update(int userId,  LoginViewModel request)
+        {
+            return Ok(await _loginService.UpdateAsync(userId, request));
+        }
+
+        [HttpDelete("userId")]
+        public async Task<IActionResult> Delete(int userId)
+        {
+            return Ok(await _loginService.DeleteAsync(userId));
+        }
     }
 }
