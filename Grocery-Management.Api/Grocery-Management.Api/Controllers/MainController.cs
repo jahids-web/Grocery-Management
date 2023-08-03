a9fe9174bd0e6cf2cbab15a96de394554bbe04df
jahids-web <jahiduddin922@gmail.com>
2023-08-04 00:10:24 +0600

    Add New Controller MainController

---
+++ Grocery-Management.Api/Grocery-Management.Api/Controllers/MainController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grocery_Management.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+    }
+}
