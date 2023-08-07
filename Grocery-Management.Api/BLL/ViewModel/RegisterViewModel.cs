dc78c3fc979d0e5cd2f3272bdf1e752a9f1fa59f
jahids-web <jahiduddin922@gmail.com>
2023-08-07 23:05:57 +0600

    Add fields

--- Grocery-Management.Api/BLL/ViewModel/RegisterViewModel.cs
+++ Grocery-Management.Api/BLL/ViewModel/RegisterViewModel.cs
@@ -29,6 +29,7 @@ namespace BLL.ViewModel
         {
             RuleFor(x => x.UserId).NotNull().NotEmpty();
             RuleFor(x => x.UserName).NotNull().NotEmpty();
+            RuleFor(x => x.Email).NotNull().NotEmpty();
             RuleFor(x => x.Address).NotNull().NotEmpty();
             RuleFor(x => x.Password).NotNull().NotEmpty();
 
