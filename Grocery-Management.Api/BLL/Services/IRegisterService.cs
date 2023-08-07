fcb2404b0454ea2f5b567c2b4b5c981dca780297
jahids-web <jahiduddin922@gmail.com>
2023-08-07 23:05:25 +0600

    Add All CRUD Method

--- Grocery-Management.Api/BLL/Services/IRegisterService.cs
+++ Grocery-Management.Api/BLL/Services/IRegisterService.cs
@@ -6,18 +6,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utility.Exceptions;
 
 namespace BLL.Services
 {
     public interface IRegisterService
     {
-        Task<Register> InsertAsync(RegisterViewModel request);
+        Task<Register> InsertAsync(RegisterViewModel requestData);
 
         Task<List<Register>> GetAllAsync();
 
         Task<Register> GetAAsync(int userId);
 
-        Task<Register> UpdateAsync(int userId, RegisterViewModel request);
+        Task<Register> UpdateAsync(int userId, RegisterViewModel requestData);
 
         Task<Register> DeleteAsync(int userId);
     }
@@ -31,29 +32,91 @@ namespace BLL.Services
             _unitOfWork = unitOfWork;
         }
 
-        public Task<Register> InsertAsync(RegisterViewModel request)
+        public async Task<Register> InsertAsync(RegisterViewModel requestData)
         {
-            throw new NotImplementedException();
+            Register aRegister = new Register();
+            aRegister.UserId = requestData.UserId;
+            aRegister.UserName = requestData.UserName;
+            aRegister.Email = requestData.Email;
+            aRegister.Address = requestData.Address;
+            aRegister.Password = requestData.Password;
+
+            try
+            {
+                await _unitOfWork.RegisterRepository.CreateAsync(aRegister);
+                if (await _unitOfWork.SavaChangesAsync())
+                {
+                    return aRegister;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new ApplicationValidationException("Insert Has Some Problem" + e);
+            }
+            return aRegister;
         }
 
-        public Task<Register> GetAAsync(int userId)
+        public async Task<Register> GetAAsync(int userId)
         {
-            throw new NotImplementedException();
+            var register = await _unitOfWork.RegisterRepository.FindSingLeAsync(x => x.UserId == userId);
+            if(register == null)
+            {
+                throw new ApplicationValidationException("Register Not Found");
+            }
+            return register;
         }
 
-        public Task<List<Register>> GetAllAsync()
+        public async Task<List<Register>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return await _unitOfWork.RegisterRepository.GetList();
         }
 
-        public Task<Register> UpdateAsync(int userId, RegisterViewModel request)
+        public async Task<Register> UpdateAsync(int userId, RegisterViewModel requestData)
         {
-            throw new NotImplementedException();
+            var register = await _unitOfWork.RegisterRepository.FindSingLeAsync(x => x.UserId == userId);
+
+            if(register == null)
+            {
+                throw new ApplicationValidationException("Register Not Found");
+            }
+            if (!string.IsNullOrWhiteSpace(requestData.UserName))
+            {
+                requestData.UserName = requestData.UserName;
+            }
+            if (!string.IsNullOrWhiteSpace(requestData.Email))
+            {
+                requestData.Email = requestData.Email;
+            }
+            if (!string.IsNullOrWhiteSpace(requestData.Password))
+            {
+                requestData.Password = requestData.Password;
+            }
+            requestData.Address = requestData.Address;
+
+            _unitOfWork.RegisterRepository.Update(register);
+            if (await _unitOfWork.SavaChangesAsync())
+            {
+                return register;
+            }
+
+            throw new ApplicationValidationException("In Update Have Some Problam");
         }
 
-        public Task<Register> DeleteAsync(int userId)
+        public async Task<Register> DeleteAsync(int userId)
         {
-            throw new NotImplementedException();
+            var register = await _unitOfWork.RegisterRepository.FindSingLeAsync(x =>x.UserId == userId);
+
+            if(register == null)
+            {
+                throw new ApplicationValidationException("Register Not Found");
+            }
+            _unitOfWork.RegisterRepository.Delete(register);
+
+            if(await _unitOfWork.SavaChangesAsync())
+            {
+               return register;
+            }
+            throw new ApplicationValidationException("Some Problem for delete data");
         }
 
     }
